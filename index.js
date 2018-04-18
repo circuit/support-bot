e4fb4451f3cd46cb8c713ea4077effc2354ddf07
Roger Urscheler <roger.urscheler@unify.com>
2018-04-18 12:36:31 -0400

    configuration

--- index.js
+++ index.js
@@ -23,6 +23,8 @@ const logger = Circuit.logger;
 config.circuit.client_id = process.env.CLIENT_ID || config.circuit.client_id;
 config.circuit.client_secret = process.env.CLIENT_SECRET || config.circuit.client_secret;
 config.circuit.domain = process.env.DOMAIN || config.circuit.domain;
+config.qna_subscription = process.env.QNA_SUBSCRIPTION || config.qna_subscription;
+console.log(config)
 client = new Circuit.Client(config.circuit);
 
 async function run() {
