879bb10fa65ee7da4241d9259fee47f777a12646
Roger Urscheler <roger.urscheler@unify.com>
2018-04-25 14:16:47 -0400

    bugs

--- server.js
+++ server.js
@@ -124,7 +124,7 @@ function processTextItem(item) {
 
   return ai.ask(question)
     .then(res => {
-      console.info('AI response abc : ', res);
+      console.info('AI response abcd : ', res);
 
       // Expects an array of objects with a 'answer' and 'score' property (0..99)
       if (!res) {
